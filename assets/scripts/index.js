d41eaa32579d3cb50d9ad2fb61ecb0eb8299dab4
kdoxsey <kdoxse@gmail.com>
2021-02-17 21:52:53 -0500

    hover colors working

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,9 @@
 'use strict'
 
 $(() => {
-    $('#test').on('click', hide('#projects'))
+    $('#test').on('click', function() {
+  alert('User clicked on "button."');
+});
 })
+
+// document.getElementById('test').addEventListener('click', alert('clicked'))
