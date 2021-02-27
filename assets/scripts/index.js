e25c01252f086339c920321cbba73d6fb50c61ff
kdoxsey <kdoxse@gmail.com>
2021-02-27 17:26:22 -0500

    email form

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,9 +1,6 @@
 'use strict'
 
 $(() => {
-    $('#test').on('click', function() {
-  alert('User clicked on "button."');
-});
 })
 
 // document.getElementById('test').addEventListener('click', alert('clicked'))
